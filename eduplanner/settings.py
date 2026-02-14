9b17c6a5288f0b5f664519c8540a01d7779db18d
Juan Goyes <juanjosegoyeszambrano@gmail.com>
2026-02-14 10:01:27 -0500

    Middleware para evitar el acceso al sistema  sin inicio de sesion, evitando que se guarde el cache

--- eduplanner/settings.py
+++ eduplanner/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "eduplanner.middleware.NoCacheMiddleware",
 ]
 
 ROOT_URLCONF = "eduplanner.urls"
